9dd1cf32b589a5533cca69ac1ec0b31d26f67790
Gianluigi Vitale <gianluigi.vitale11@gmail.com>
2020-04-03 16:50:39 +0200

    only possible range jan 2018 - dec 2018 (functions). Added js comments

--- js/main.js
+++ js/main.js
@@ -9,28 +9,25 @@ $(document).ready(function() {
     stampaFestivi(dataIniziale);
 
 
-
-
-    $('.mese-successivo').click(function () {
-        dataIniziale.add(1, 'month');
+    $('.mese-successivo').click(function () {   // quando si clicca per andare al mese successivo
+        controlloMeseNext(dataIniziale);
         stampoGiorniMese(dataIniziale);
         stampaFestivi(dataIniziale);
     });
 
-    $('.mese-precedente').click(function () {
-        dataIniziale.subtract(1, 'month');
+    $('.mese-precedente').click(function () {   // quando si clicca per tornare al mese precedente
+        controlloMesePrevt(dataIniziale);
         stampoGiorniMese(dataIniziale);
         stampaFestivi(dataIniziale);
     });
 
-    //
 
 
     // FUNZIONI USATE
 
 
 
-    function stampaFestivi(meseDaStampare) {
+    function stampaFestivi(meseDaStampare) {    // questa funzione fa una chiamata AJAX a un API e se c'e' una vacanza in un giorno cambia il colore di quel giorno e dice quale e' la vacanza
         var giornoX = meseDaStampare.clone();
         var meseAttuale = giornoX.month();
         $.ajax({
@@ -53,7 +50,7 @@ $(document).ready(function() {
     }
 
 
-    function stampoGiorniMese(meseDaStampare) {
+    function stampoGiorniMese(meseDaStampare) {         // questa funzione prende dalla variabile meseDaStampare il mese, vede quanti giorni ci sono e stampa a schermo tutti i giorni (handlebars)
         $('#giorni-mese').empty();
         var giorniMese = meseDaStampare.daysInMonth();
         var giornoX = meseDaStampare.clone();
@@ -71,6 +68,28 @@ $(document).ready(function() {
             var templateFinale = templateGiorno(giornoDaInserire); // Stiamo popolando il template con i dati dell'oggetto
             $('#giorni-mese').append(templateFinale);
             giornoX.add(1, 'day');
+        }
+    }
+
+
+    function controlloMeseNext(meseDaStampare) {        // questa funzione fa si che si ci muova sempre tra il range gennaio 2018 e dicembre 2018 se il mese e' dicembre torna a gennaio
+        var giornoX = meseDaStampare.clone();
+        var meseAttuale = giornoX.format('MMMM');
+        if (meseAttuale == 'dicembre') {
+            dataIniziale.subtract(11, 'month');
+        } else {
+            dataIniziale.add(1, 'month');
+        }
+    }
+
+
+    function controlloMesePrevt(meseDaStampare) {       // questa funzione fa si che si ci muova sempre tra il range gennaio 2018 e dicembre 2018 se il mese e' gennaio torna a dicembre
+        var giornoX = meseDaStampare.clone();
+        var meseAttuale = giornoX.format('MMMM');
+        if (meseAttuale == 'gennaio') {
+            dataIniziale.add(11, 'month');
+        } else {
+            dataIniziale.subtract(1, 'month');
         }
     }
 
