c58a949c137946ca134cf4446bb88590654edcf1
Gianluigi Vitale <gianluigi.vitale11@gmail.com>
2020-04-03 15:59:36 +0200

    draft html css, given a date it prints days of the month

--- js/main.js
+++ js/main.js
@@ -1,2 +1,23 @@
-var test = moment().format();
-console.log(test);
+var htmlGiorno = $('#calendar-template').html();
+var templateGiorno = Handlebars.compile(htmlGiorno);
+
+
+var dataIniziale = moment('01-01-2018', 'DD-MM-YYYY');
+stampoGiorniMese(dataIniziale)
+
+function stampoGiorniMese(meseDaStampare) {
+    $('#giorni-mese').empty();
+    var giorniMese = meseDaStampare.daysInMonth();
+    var giornoX = meseDaStampare.clone();
+    var meseAttuale = giornoX.format('MMMM');
+    $('#mese').text(meseAttuale);
+
+    for (var i = 1; i <= giorniMese; i++) {
+        var giornoDaInserire = {
+            giorno: i + ' ' + meseAttuale
+        }
+
+        var templateFinale = templateGiorno(giornoDaInserire); // Stiamo popolando il template con i dati dell'oggetto
+        $('#giorni-mese').append(templateFinale);
+    }
+}
